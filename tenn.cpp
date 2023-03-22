162d58a78df8378a46a349e1fe32bb9f5f522191
hyrinn <126777014+hyrinn@users.noreply.github.com>
2023-03-22 17:23:31 +0900

    Add files via upload

---
+++ tenn.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cout << "문자열 입력>>";
+    getline(cin, s);
+
+    for(int i=0; i < s.length(); i++){
+        for(int j=0; j <= i; j++){
+            cout << s[j];
+        }
+        cout <<'\n';
+    }
+}
